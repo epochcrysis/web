6e146750c9d67ea2c8277a5d5c9e0c55fe1ed1b1
Andrew Ghostuhin <me@torinasakura.name>
2019-08-03 12:58:48 +0300

    feat(frontend): remove wp-multilang and use instead polylang
    affects: @site/app
    
    fix wp-version, also add apollo locale params

--- frontend/site/app/src/providers/apollo.ts
+++ frontend/site/app/src/providers/apollo.ts
@@ -2,7 +2,7 @@ import { withApollo } from '@monstrs/next-app-with-apollo'
 
 export const apollo = withApollo({
   url: ({ locale }) =>
-    'https://wp.local.epochcrysis.band/graphql/?lang=${locale}',
+    `https://wp.local.epochcrysis.band/graphql/?lang=${locale}`,
 })
 
 // local network and external network
