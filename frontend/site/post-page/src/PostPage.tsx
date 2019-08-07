1d218fc79566eba21f2d5bd187200cda56235a0e
Andrew Ghostuhin <me@torinasakura.name>
2019-08-07 12:36:14 +0300

    feat(frontend): complete post page
    affects: @site/post-page

--- frontend/site/post-page/src/PostPage.tsx
+++ frontend/site/post-page/src/PostPage.tsx
@@ -4,12 +4,17 @@ import { DrawerProvider } from '@ui/navigation'
 import { StickyContainer } from '@ui/sticky'
 import { Navigation } from '@site/navigation'
 import { Footer } from '@site/footer'
+import { Content } from './Content'
 
-export const PostPage = () => (
+export const PostPage = ({ router }) => (
   <StickyContainer>
     <DrawerProvider>
       <Column>
         <Navigation />
+        <Layout basis={['80px', '120px', '120px']} />
+        <Layout>
+          <Content slug={router.query.slug} />
+        </Layout>
         <Layout basis={['80px', '120px', '120px']} />
         <Layout>
           <Footer />
