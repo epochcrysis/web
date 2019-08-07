65a1c22837bcdcbbcc97fdc13ee3192214bf7497
Andrew Ghostuhin <me@torinasakura.name>
2019-08-07 12:42:34 +0300

    style(frontend): drawer styling
    affects: @site/navigation

--- frontend/site/navigation/src/Drawer.tsx
+++ frontend/site/navigation/src/Drawer.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Column, Layout } from '@ui/layout'
+import { Box, Column, Layout } from '@ui/layout'
 import { Drawer as UIDrawer } from '@ui/navigation'
 import { NextNavLink as NavLink } from '@ui/link'
 
 export const Drawer = ({ items = [] }) => (
-  <UIDrawer>
-    <Column>
-      <Layout basis={20} />
-      {items.map(item => (
-        <Layout key={item.id}>
-          <NavLink href={item.url}>{item.label}</NavLink>
-        </Layout>
-      ))}
-    </Column>
-  </UIDrawer>
+  <Box display={['flex', 'none', 'none']}>
+    <UIDrawer>
+      <Column>
+        <Layout basis={20} />
+        {items.map(item => (
+          <Layout key={item.id}>
+            <NavLink href={item.url}>{item.label}</NavLink>
+          </Layout>
+        ))}
+      </Column>
+    </UIDrawer>
+  </Box>
 )
